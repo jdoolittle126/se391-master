e200496b3e7f09729deb6a0b21180f2aff71e60e
Jonathan <jdoolittle126@gmail.com>
2021-09-15 15:14:16 -0400

    Week 9

---
+++ w8-lab-8/app/src/main/java/edu/neit/jonathandoolittle/candylab/CandyOrderDatabase.kt
@@ -0,0 +1,32 @@
+package edu.neit.jonathandoolittle.candylab
+
+/**
+ * 
+ * Class Description - TODO
+ *
+ * Class Logic - TODO
+ *
+ * <pre>
+ *  Class Usage - TODO
+ * </pre>
+ *
+ * @author Jonathan Doolittle
+ * @version 0.1 - 9/15/2021
+ *
+ */
+
+class CandyOrderDatabase {
+
+	// ******************************
+	// Variables
+	// ******************************
+	
+	// ******************************
+	// Public methods
+	// ******************************
+	
+	// ******************************
+	// Private methods
+	// ******************************
+
+}
